f4f732f18d091e3b213f0d496aee0dc937648c6e
Sebastian Winkler <sekl@slmn.de>
2017-07-31 01:15:05 +0200

    adds profile embeds on hover

--- src/RobyulWebBundle/Resources/public/js/main.js
+++ src/RobyulWebBundle/Resources/public/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+    $('.server-tile-item').each(function() {
+        var $tileItem = $(this);
+        var iframeUrl = $tileItem.data('profile-iframe-url');
+
+        $tileItem.popover({
+            content: "<iframe width=\"400\" height=\"300\" frameborder=\"0\" scrolling=\"no\" src=\"" + iframeUrl + "\"></iframe>",
+            html: true,
+            trigger: "hover"
+        });
+    });
+});
