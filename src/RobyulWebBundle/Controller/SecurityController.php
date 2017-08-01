64eccf5bb68e11a9083a4ff43a8ad479b2575b0e
Sebastian Winkler <sekl@slmn.de>
2017-08-01 23:54:41 +0200

    adds caching to profile page

--- src/RobyulWebBundle/Controller/SecurityController.php
+++ src/RobyulWebBundle/Controller/SecurityController.php
@@ -3,6 +3,8 @@
 namespace RobyulWebBundle\Controller;
 
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use MessagePack\Packer;
+use MessagePack\Unpacker;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Unirest;
@@ -21,12 +23,36 @@ class SecurityController extends Controller
             ->addMeta('property', 'og:description', "View your Profile.");
         $seoPage->addMeta('property', 'og:title', $seoPage->getTitle());
 
-        $allGuilds = Unirest\Request::get('http://localhost:2021/bot/guilds');
+        $unpacker = new Unpacker();
+        $packer = new Packer();
+        $redis = $this->container->get('snc_redis.default');
+
+        $key = 'robyul2-web:api:bot:guilds';
+        if ($redis->exists($key) == true) {
+            $allGuilds = unserialize($unpacker->unpack($redis->get($key)));
+        } else {
+            $allGuilds = Unirest\Request::get('http://localhost:2021/bot/guilds');
+            $allGuilds = (array) $allGuilds->body;
+
+            $redis->set($key, $packer->pack(serialize($allGuilds)));
+            $redis->expireat($key, strtotime("+15 minutes"));
+        }
+
         $isInGuilds = array();
 
-        foreach ($allGuilds->body as $guild) {
-            $member = Unirest\Request::get('http://localhost:2021/member/'.$guild->ID.'/'.$this->getUser()->getID().'/is');
-            if ($member->body->IsMember === true) {
+        foreach ($allGuilds as $guild) {
+            $key = 'robyul2-web:api:member:'.$guild->ID.':'.$this->getUser()->getID().':is';
+            if ($redis->exists($key) == true) {
+                $isMember = $unpacker->unpack($redis->get($key));
+            } else {
+                $isMember = Unirest\Request::get('http://localhost:2021/member/'.$guild->ID.'/'.$this->getUser()->getID().'/is');
+                $isMember = (bool) $isMember->body->IsMember;
+
+                $redis->set($key, $packer->pack($isMember));
+                $redis->expireat($key, strtotime("+15 minutes"));
+            }
+
+            if ($isMember === true) {
                 $isInGuilds[] = $guild;
             }
         }
