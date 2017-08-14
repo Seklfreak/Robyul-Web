42b67a133390da8ab27ad638786b4080da3aa17d
Sebastian Winkler <sebastian.winkler@slmn.de>
2017-08-15 00:42:58 +0200

    gets profile data from the api, placeholder if no profile image

--- src/RobyulWebBundle/Twig/DiscordExtension.php
+++ src/RobyulWebBundle/Twig/DiscordExtension.php
@@ -15,13 +15,36 @@ class DiscordExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
+            new \Twig_Function('discord_user', array($this, 'getDiscordUser')),
             new \Twig_Function('discord_avatar', array($this, 'getDiscordAvatar')),
             new \Twig_Function('user_rank_data', array($this, 'getUserRankData')),
         );
     }
 
+    public function getDiscordUser($userID)
+    {
+        $redis = $this->container->get('snc_redis.default');
+
+        $key = 'robyul2-web:api:user:' . $userID;
+        if ($redis->exists($key) == true) {
+            $userData = unserialize($redis->get($key));
+        } else {
+            $userInfo = Unirest\Request::get('http://localhost:2021/user/' . $userID);
+            $userData = (array)$userInfo->body;
+
+            $redis->set($key, serialize($userData));
+            $redis->expireat($key, strtotime("+15 minutes"));
+        }
+
+        return $userData;
+    }
+
     public function getDiscordAvatar($userID, $hash)
     {
+        if ($hash == '') {
+            return $this->container->get('assets.packages')->getUrl('static/images/placeholder_icon.jpg');
+        }
+
         if (substr($hash, 0, 2) === "a_") {
             return 'https://cdn.discordapp.com/avatars/' . $userID . '/' . $hash . '.gif';
         } else {
