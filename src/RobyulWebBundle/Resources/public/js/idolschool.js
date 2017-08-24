45f9d6d8e6a29d8905658079dfac68e59ca473f6
Sebastian Winkler <sebastian.winkler@slmn.de>
2017-08-25 00:10:28 +0200

    updates idol school

--- src/RobyulWebBundle/Resources/public/js/idolschool.js
+++ src/RobyulWebBundle/Resources/public/js/idolschool.js
@@ -15,7 +15,7 @@ var middlePadding = (padding * 2) + 100;
 //var width = $(window).width() - middlePadding - CHART_WIDTH - 30;
 var width = 1280 - middlePadding - CHART_WIDTH - 30;
 
-var episodes = [1, 2, 3, 4, 5];
+var episodes = [1, 2, 3, 4, 5, 6];
 var totalData;
 var dFirst;
 
@@ -32,7 +32,7 @@ var plot = svg.append("g").attr("transform", "translate(" + padding + "," + padd
 setXAxis();
 
 // Get data
-d3.csv("/bundles/robyulweb/csv/idolschool.v5.csv", parseLine, function (err, data) {
+d3.csv("/bundles/robyulweb/csv/idolschool.v6.csv", parseLine, function (err, data) {
     totalData = processData(data);
     plotData(data);
     selectLine(dFirst, "#line1");
