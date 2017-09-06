14572fca7609a224bbfae1fdfefd00926c40868c
Sebastian Winkler <sebastian.winkler@slmn.de>
2017-09-06 15:24:23 +0200

    use file hashes instead

--- src/RobyulWebBundle/Controller/RandomPicturesProxy.php
+++ src/RobyulWebBundle/Controller/RandomPicturesProxy.php
@@ -18,9 +18,51 @@ class RandomPicturesProxy extends Controller
      *     }
      * )
      */
-    public function proxyAction($sourceID, $pictureID, $slug, $_format)
+    public function proxyByNAction($sourceID, $pictureID, $slug, $_format)
     {
-        $key = "robyul2-discord:randompictures:filescache:".$sourceID.":entry:".$pictureID;
+        $key = "robyul2-discord:randompictures:filescache:by-n:".$sourceID.":entry:".$pictureID;
+
+        $logger = $this->get('logger');
+        $logger->info("looking up \"".$key."\"");
+
+        $redis = $this->container->get('snc_redis.default');
+        $result = $redis->get($key);
+
+        if ($result != "") {
+            $unpacker = new Unpacker();
+            $gdFile = $unpacker->unpack($result);
+
+            $dlLink = 'https://drive.google.com/uc?id='.$gdFile['Id'].'&export=download';
+            $logger->info("downloading \"".$dlLink."\"");
+
+            ini_set('max_execution_time', 300);
+            ini_set('default_socket_timeout', 100);
+            $binary = file_get_contents($dlLink);
+
+            return new Response(
+                $binary,
+                Response::HTTP_OK,
+                array(
+                    'Content-Type' => 'image/'.$_format,
+                    'Content-Disposition' => 'inline; filename="'.$slug.'.'.$_format.'"'
+                )
+            );
+        } else {
+            throw $this->createNotFoundException('Image not found.');
+        }
+    }
+
+    /**
+     * @Route(
+     *     "/static/proxy/{fileHash}/{slug}.{_format}",
+     *     requirements={
+     *         "_format": "jpeg|jpg|gif|png"
+     *     }
+     * )
+     */
+    public function proxyByHashAction($fileHash, $slug, $_format)
+    {
+        $key = "robyul2-discord:randompictures:filescache:by-hash:".$fileHash;
 
         $logger = $this->get('logger');
         $logger->info("looking up \"".$key."\"");
