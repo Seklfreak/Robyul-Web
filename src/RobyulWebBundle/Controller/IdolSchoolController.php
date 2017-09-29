32cbab91adc5ed318ccd3672d145a99aed714528
Sebastian Winkler <sebastian.winkler@slmn.de>
2017-09-29 23:41:04 +0200

    updates idol school

--- src/RobyulWebBundle/Controller/IdolSchoolController.php
+++ src/RobyulWebBundle/Controller/IdolSchoolController.php
@@ -14,7 +14,7 @@ class IdolSchoolController extends Controller
     {
         $seoPage = $this->container->get('sonata.seo.page');
         $seoPage
-            ->setTitle("Idol School (fromis-) Ranking - The KPop Discord Bot - Robyul")
+            ->setTitle("Idol School (fromis_) Ranking - The KPop Discord Bot - Robyul")
             ->addMeta('name', 'description', "View the Ranking over time for Mnet's Idol School forming the Girl Group fromis_.")
             ->addMeta('property', 'og:description', "View the Ranking over time for Mnet's Idol School forming the Girl Group fromis_.");
         $seoPage->addMeta('property', 'og:title', $seoPage->getTitle());
