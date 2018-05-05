4da35d0568ecc0a7abee63250e71b9c821a38375
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-05-05 15:23:22 +0200

    fixes intruders spots

--- src/RobyulWebBundle/Resources/public/js/produce101china.js
+++ src/RobyulWebBundle/Resources/public/js/produce101china.js
@@ -15,7 +15,7 @@ var middlePadding = (padding * 2) + 100;
 //var width = $(window).width() - middlePadding - CHART_WIDTH - 30;
 var width = 1280 - middlePadding - CHART_WIDTH - 30;
 
-var csv = "produce101china.v2.csv";
+var csv = "produce101china.v3.csv";
 var episodes = [2,3];
 var totalData;
 var dFirst;
