52c6a55fe23d7975406b773b4771e952a387630f
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-05-13 15:46:47 +0200

    updates produce 101 china ranking

--- src/RobyulWebBundle/Resources/public/js/produce101china.js
+++ src/RobyulWebBundle/Resources/public/js/produce101china.js
@@ -15,8 +15,8 @@ var middlePadding = (padding * 2) + 100;
 //var width = $(window).width() - middlePadding - CHART_WIDTH - 30;
 var width = 1280 - middlePadding - CHART_WIDTH - 30;
 
-var csv = "produce101china.v3.csv";
-var episodes = [2,3];
+var csv = "produce101china.v4.csv";
+var episodes = [2,3,4];
 var totalData;
 var dFirst;
 
